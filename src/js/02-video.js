01f1194c28afe69631f65a21837d011651fceb34
Савицька Катерина <savytskaa.kate@gmail.com>
2023-02-06 09:09:31 +0200

    HW 08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,7 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle(function (data) {
     const time = data.seconds;
     console.log(time);
-   localStorage.setItem(TIME_DATA_PLAYER, time)
+   localStorage.setItem('videoplayer-current-time', time)
 },1000));
 
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time')||0);
